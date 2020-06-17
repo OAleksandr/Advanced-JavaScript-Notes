3b409e9906e342cc78747167e1bbaae696a2226a
OAleksandr <42975228+OAleksandr@users.noreply.github.com>
2020-06-17 13:40:40 -0600

    React Stock API App Added

---
+++ React Stock API/js/src/components/StockHistoryDisplay.js
@@ -0,0 +1,34 @@
+const StockHistoryDisplay = (props) => {
+
+    React.useEffect(() => {
+        console.log("History");
+        console.log(props.history);
+    },[props.history]);
+
+     //Currency Formatter
+     const currencyFormatter = (value) => {
+        return (+value).toLocaleString('en-US', {
+            style : 'currency',
+            currency : 'USD'
+        });
+    }
+
+    return <div>
+                <React.Fragment>
+                    <h2>Five Days History for {props.smbl}</h2>
+                            {props.history.map(fiveDays => 
+                            <div className="history-data" key={fiveDays.date}>
+                                <h4><strong>Date: </strong>{fiveDays.date}</h4>
+                                <ul>
+                                    <li><strong>Open: </strong>{currencyFormatter(fiveDays.open)}</li>
+                                    <li><strong>Low: </strong>{currencyFormatter(fiveDays.low)}</li>
+                                    <li><strong>High: </strong>{currencyFormatter(fiveDays.high)}</li>
+                                    <li><strong>Close: </strong>{currencyFormatter(fiveDays.close)}</li>
+                                </ul>
+                            </div>
+                            )}
+                </React.Fragment>
+            </div>
+}
+
+export default StockHistoryDisplay;
---
+++ React Stock API/js/src/components/StockSearchForm.js
@@ -0,0 +1,24 @@
+const StockSearchForm = (props) => {
+
+    const [stockSymbol, setStockSymbol] = React.useState("");
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        props.setStockCallback(stockSymbol);
+    }
+
+    const onChangeHandler = (event) => {
+        setStockSymbol(event.target.value);
+    }
+
+    return <div>
+        <h2>Stock Search Form</h2>
+        <form className="frm symbol" onSubmit={onSubmitHandler}>
+            <label htmlFor="symbol">Ticker Symbol</label>
+            <input type="text" id="symbol" name="symbol" onChange={onChangeHandler} value={stockSymbol}/>
+            <button type="submit">View</button>
+        </form>
+    </div>
+}
+
+export default StockSearchForm;
